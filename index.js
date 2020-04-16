a593696a03b07e8c74aec3c9c659e201b10097a4
mybells <suninfo@mybellsdeMac-mini.local>
2020-04-16 15:08:50 +0800

    add:测试模态框插件

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 /**
  * @format
  */
+import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 
+/* example */
+import ModalTester from './src/pages/modal';
+
+class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          {/* 模态框 */}
+          <ModalTester />
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
 AppRegistry.registerComponent(appName, () => App);
