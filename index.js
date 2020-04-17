500a85518a0160517fe6510548f3e45dc7f0c67f
mybells <suninfo@mybellsdeMac-mini.local>
2020-04-17 13:29:35 +0800

    add: react-navigation

--- index.js
+++ index.js
@@ -3,22 +3,29 @@
  */
 import React from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
-
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+
 /* example */
-import ModalTester from './src/pages/modal';
+import ModalTester from './src/pages/ModalTester';
+import RootNavigator from './src/pages/RootNavigator';
 
 class App extends React.Component {
   render() {
     return (
-      <SafeAreaView>
-        <ScrollView>
-          {/* 模态框 */}
-          <ModalTester />
-        </ScrollView>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView>
+          <ScrollView>
+            {/* 模态框 */}
+            <ModalTester />
+            {/* 导航 */}
+          </ScrollView>
+        </SafeAreaView>
+        <RootNavigator />
+      </NavigationContainer>
     );
   }
 }
---
+++ src/pages/RootNavigator.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {Button, View, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+/* react-navigation 5.x */
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default RootNavigator;
